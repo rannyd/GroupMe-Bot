128a510bc8fbb62c9488aa21e558e0729df9582f
rannyd <73037662+rannyd@users.noreply.github.com>
2020-10-17 16:54:57 -0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
-exports.ACCESS_TOKEN = "GroupMe access token";
-exports.BOT_ID = "GroupMe bot ID";
-exports.GROUP_ID = "GroupMe group id";
+exports.ACCESS_TOKEN = "ZF6et8Hj9uh9wVGqCDsQVg3lzjqtxTCx413S7nzB";
+exports.BOT_ID = "9e11236a05426a5a0fcaf2568e";
+exports.GROUP_ID = "63458814";
 exports.GIHPY_KEY = 'Giphy API key';
 
 exports.DEFAULT_MSGS_TO_ANALYZE = "2000";
